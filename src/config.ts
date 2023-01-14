5762df29f30b0495f46d961fc937cb9259eacd7b
Lucas-Feat <lucas@nexos.bzh>
2023-01-14 18:59:16 +0100

    export credentials & secrets

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 import {ServiceAccount} from 'firebase-admin';
 
 export const config: any = JSON.parse(Buffer.from(process.env.CONFIG, 'base64').toString());
-export const serviceAccount: ServiceAccount = JSON.parse(Buffer.from(process.env.GCP_CREDENTIALS, 'base64').toString()) as ServiceAccount;
+export const serviceAccount: ServiceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString());
